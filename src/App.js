236a0b9e8cfd26786a95da11641ada4e59e1eb7c
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2025-07-01 14:16:18 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import ManuscriptCreatePage from './pages/ManuscriptCreatePage';
+import ManuscriptEditPage from './pages/ManuscriptEditPage';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+        <Routes>
+          <Route path="/manuscripts/create" element={<ManuscriptCreatePage />} />
+          <Route path="/manuscripts/edit/:bookId" element={<ManuscriptEditPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
